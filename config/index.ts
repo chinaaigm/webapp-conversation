eb83fef7a1cb3250731731d021caf76824d7c3da
chinagm <31269709+chinaaigm@users.noreply.github.com>
2025-01-08 17:43:16 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,14 +3,14 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
-  privacy_policy: '',
-  default_language: 'en',
+  title: '小爱同学',
+  description: '小爱同学',
+  copyright: 'Google',
+  privacy_policy: 'https://aichina.us.kg/',
+  default_language: 'zh',
 }
 
-export const isShowPrompt = false
+export const isShowPrompt = true
 export const promptTemplate = 'I want you to act as a javascript console.'
 
 export const API_PREFIX = '/api'
